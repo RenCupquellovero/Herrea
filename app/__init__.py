19059120395a5eb1e816b79d8b4d5e5fed1422e0
RenCup <151194969+RenCupquellovero@users.noreply.github.com>
2026-03-26 20:56:27 +0100

    Update app/__init__.py to add todos_bp blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config_loader import load_config
 from .routes_public import public_bp
 from .routes_admin import admin_bp
+from .routes_todos import todos_bp
 
 config = load_config()
 
@@ -14,5 +15,6 @@ def create_app():
 
     app.register_blueprint(public_bp, url_prefix=base + pub)
     app.register_blueprint(admin_bp, url_prefix=base + adm)
+    app.register_blueprint(todos_bp, url_prefix=base + adm)
 
     return app
